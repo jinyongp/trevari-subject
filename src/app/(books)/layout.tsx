e709e72dce33e844146d9f06618c5c6bfa721cd9
jinyongp <dev.jinyongp@gmail.com>
2023-12-16 05:40:15 +0900

    style: footer 높이 조절

--- src/app/(books)/layout.tsx
+++ src/app/(books)/layout.tsx
@@ -5,8 +5,8 @@ export default function HomeLayout({ children }: React.PropsWithChildren) {
         <h1 className="text-3xl font-extrabold text-slate-900 md:text-4xl">도서 검색 서비스</h1>
         <h2 className="text-sm font-bold text-slate-500 md:text-lg">검색하고 싶은 책의 제목을 입력해보세요!</h2>
       </header>
-      <main className="container mx-auto pb-20">{children}</main>
-      <footer className="absolute bottom-0 grid h-20 w-full place-items-center gap-4">
+      <main className="container mx-auto pb-20 md:pb-32">{children}</main>
+      <footer className="absolute bottom-0 grid h-20 w-full place-items-center gap-4 md:h-32">
         <p className="text-sm font-bold text-slate-500 md:text-lg">
           <a href="https://itbook.store/">itbook.store</a>
         </p>
