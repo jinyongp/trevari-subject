6541c83330fbeb7095f4c8a2817ebf2a462cf1fb
jinyongp <dev.jinyongp@gmail.com>
2023-12-18 16:38:39 +0900

    chore: 불필요한 코드 삭제

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,9 +1,5 @@
 import SearchProvider from "./search-provider";
 
 export default function Provider({ children }: React.PropsWithChildren) {
-  return (
-    // <ScrollProvider>
-    <SearchProvider>{children}</SearchProvider>
-    // </ScrollProvider>
-  );
+  return <SearchProvider>{children}</SearchProvider>;
 }
